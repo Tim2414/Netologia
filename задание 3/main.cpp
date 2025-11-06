45ffa7acf7b88885bfa3779f59a2aeb1a6f040dd
Tim2414 <gfh725577@gmail.com>
2025-11-06 21:46:15 +0300

    3

---
+++ задание 3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <Windows.h>
+
+int fibonacciLite(int num );
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    int num{};
+
+    std::cout << "Введите число: ";
+    std::cin >> num;
+    std::cout << "Числа Фибоначчи:";
+
+    if (num > 2)
+    {
+        for (int count{}; count < num; count++)
+        {
+            std::cout << " " << fibonacciLite(count);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int fibonacciLite(int num)
+{
+    if (num == 0) { return 0; }
+    if (num == 1) { return 1; }
+
+    return fibonacciLite(num - 1) + fibonacciLite(num - 2);
+}
