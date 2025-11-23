46d9b5fe6e38bb02a1c0c1d16293e94ff528ceaa
Tim2414 <gfh725577@gmail.com>
2025-11-23 23:20:40 +0300

    задание 3

---
+++ задача 3/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <Windows.h>
+void reverse(int* array, int size);
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+
+    int size{10};
+    int array[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    std::cout << "До функции reverse: ";
+    for(int counter{}; counter < size; counter++)
+    {
+        std::cout << array[counter] << " ";
+    }
+    std::cout << std::endl;
+
+    reverse(array, size);
+    std::cout << "После функции reverse: ";
+    for(int counter{}; counter < size; counter++)
+    {
+        std::cout << array[counter] << " ";
+    }
+    std::cout << std::endl;
+
+    return EXIT_SUCCESS;
+}
+
+void reverse(int* array, int size)
+{
+    int arrayReverse[size];
+    int counterRevers{0};
+
+    for(int counter{}; counter < size; counter++)
+    {
+        arrayReverse[counter] = array[counter];
+    }
+
+    for(int counter{size-1}; counter >= 0; counter--)
+    {
+        array[counterRevers] = arrayReverse[counter];
+        ++counterRevers;
+    }
+}
